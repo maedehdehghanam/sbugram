505bbb527aa3ae0148d27ccd3e7bc2063d860572
unofficialbug <maedehdehghanam@gmail.com>
2021-06-23 14:07:31 +0430

    add like and unlike client API

--- src/sbu/client/API.java
+++ src/sbu/client/API.java
@@ -12,7 +12,7 @@ public class API{
 		toSend.put("command", Command.CHECK_USERNAME);
 		toSend.put("username",check);
 		Map<String,Object> recieved = Connector.serve(toSend);
-		return (boolean) recieved.get("answer");
+		return (boolean) recieved.get("success");
 	}
 	public static Profile login(String username, String password){
 		Map<String,Object> toSend = new HashMap<>();
@@ -20,23 +20,23 @@ public class API{
 		toSend.put("username",username);
 		toSend.put("password",password);
 		Map<String,Object> recieved = Connector.serve(toSend);
-		if ( recieved.get("answer") == null ) return null;
-		return (Profile) recieved.get("answer");
+		if ( recieved.get("success") == null ) return null;
+		return (Profile) recieved.get("success");
 	}
 	public static Boolean signUp(Profile profile){
 		Map<String,Object> toSend = new HashMap<>();
 		toSend.put("command", Command.SIGNUP);
 		toSend.put("profile", profile);
 		Map<String,Object> recieved = Connector.serve(toSend);
-		if ( recieved.get("answer") == null ) return null;
-		return (Boolean) recieved.get("answer");
+		if ( recieved.get("success") == null ) return null;
+		return (Boolean) recieved.get("success");
 	}
 	public static Boolean logout(){
 		Map<String,Object> toSend = new HashMap<>();
 		toSend.put("command", Command.LOGOUT);
 		Map<String,Object> recieved = Connector.serve(toSend);
-		if ( recieved.get("answer") == null ) return false;
-		return (Boolean) recieved.get("answer");
+		if ( recieved.get("success") == null ) return false;
+		return (Boolean) recieved.get("success");
 	}
 	public static void sendPost(Post thePost){
 		Map<String,Object> toSend = new HashMap<>();
@@ -44,4 +44,21 @@ public class API{
 		toSend.put("post",thePost);
 		Connector.serve(toSend);
 	}
+	public static Boolean like(Post post,Profile profile){
+		Map<String,Object> toSend = new HashMap<>();
+		toSend.put("profile",profile);
+		toSend.put("post", post);
+		toSend.put("command", Command.LIKE);
+		Map<String,Object> recieved = Connector.serve(toSend);
+		return (boolean) recieved.get("success");
+	}
+	public static Boolean unlike(Post post,Profile profile){
+		Map<String,Object> toSend = new HashMap<>();
+		toSend.put("profile",profile);
+		toSend.put("post", post);
+		toSend.put("command", Command.UNLIKE);
+		Map<String,Object> recieved = Connector.serve(toSend);
+		return (boolean) recieved.get("success");
+	}
+	
 }
