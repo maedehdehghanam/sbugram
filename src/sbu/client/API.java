3dbd1de62138d180a966964124fc05e958f2e9f7
unofficialbug <maedehdehghanam@gmail.com>
2021-06-23 12:08:55 +0430

    add client APIs

--- src/sbu/client/API.java
+++ src/sbu/client/API.java
@@ -7,5 +7,28 @@ import java.net.*;
 import java.util.*;
 import java.util.concurrent.*;
 public class API{
-	
+	public static boolean doesUserNameExists(String check){
+		Map<String,Object> toSend = new HashMap<>();
+		toSend.put("command", Command.CHECK_USERNAME);
+		toSend.put("username",check);
+		Map<String,Object> recieved = Connector.serve(toSend);
+		return (boolean) recieved.get("answer");
+	}
+	public static Profile login(String username, String password){
+		Map<String,Object> toSend = new HashMap<>();
+		toSend.put("command", Command.LOGIN);
+		toSend.put("username",username);
+		toSend.put("password",password);
+		Map<String,Object> recieved = Connector.serve(toSend);
+		if ( recieved.get("answer") == null ) return null;
+		return (Profile) recieved.get("answer");
+	}
+	public static Boolean signUp(Profile profile){
+		Map<String,Object> toSend = new HashMap<>();
+		toSend.put("command", Command.SIGNUP);
+		toSend.put("profile", profile);
+		Map<String,Object> recieved = Connector.serve(toSend);
+		if ( recieved.get("answer") == null ) return null;
+		return (Boolean) recieved.get("answer");
+	}
 }
