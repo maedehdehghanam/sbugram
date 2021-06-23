568165a4a6412ea296bf054804854c5cc49383bb
unofficialbug <maedehdehghanam@gmail.com>
2021-06-23 16:27:08 +0430

    add fillingfields label

--- src/sbu/client/controller/Page1Controller.java
+++ src/sbu/client/controller/Page1Controller.java
@@ -8,12 +8,16 @@ public class Page1Controller {
     
     @FXML
     private Label label;
+    @FXML
+    private Label fillFields;
     @FXML 
     private Button loginButton;
     @FXML
     private TextField userField;
     @FXML
     private PasswordField passField;
+    @FXML
+    private Button forgotPassButton;
     public void initialize() {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
@@ -21,9 +25,14 @@ public class Page1Controller {
     } 
     //fucking comment
     public void loginController(ActionEvent event){
+        label.setVisible(false);
+        fillFields.setVisible(false);
         String usernameCheck = userField.getText();
         String passwordCheck =  passField.getText();
-        if(usernameCheck.equals("ali") && passwordCheck.equals("1111")){
+        if(usernameCheck.isEmpty()|| passwordCheck.isEmpty()){
+            fillFields.setVisible(true);
+        }
+        else if(usernameCheck.equals("ali") && passwordCheck.equals("1111")){
             System.out.println("hey!");
             Alert alert = new Alert(Alert.AlertType.INFORMATION,"WELLCOME!");
             alert.showAndWait();
