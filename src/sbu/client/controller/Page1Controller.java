3d6eb41dbde0827bdf7b0a16ed78b18727d7252c
unofficialbug <maedehdehghanam@gmail.com>
2021-06-23 15:55:37 +0430

    some ui works for login page

--- src/sbu/client/controller/Page1Controller.java
+++ src/sbu/client/controller/Page1Controller.java
@@ -19,9 +19,18 @@ public class Page1Controller {
         String javafxVersion = System.getProperty("javafx.version");
         //label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
     } 
+    //fucking comment
     public void loginController(ActionEvent event){
-        System.out.println(userField.getText() + " " + passField.getText());
-        sbu.client.Main.fxmlYarooKone("timeline.fxml");
-
+        String usernameCheck = userField.getText();
+        String passwordCheck =  passField.getText();
+        if(usernameCheck.equals("ali") && passwordCheck.equals("1111")){
+            System.out.println("hey!");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION,"WELLCOME!");
+            alert.showAndWait();
+        }else{
+            label.setVisible(true);
+        }
+        //sbu.client.Main.fxmlYarooKone("timeline.fxml");
+        //wait(100000);
     } 
 }
